96feb060afd3100a271275e9ed6bfb5af41e2051
Owen Davis-Bower <davisbom@whitman.edu>
2020-03-11 18:54:47 -0700

    Clean up gaussian.py

--- src/gaussian.py
+++ src/gaussian.py
@@ -13,7 +13,7 @@ import imageutilities as iu
 from matplotlib.patches import Ellipse
 
 def make_model(X):
-    GMM = BayesianGaussianMixture(n_components=100, covariance_type='spherical', weight_concentration_prior=0.001/100, verbose=2).fit(X) # Instantiate and fit the model
+    GMM = BayesianGaussianMixture(n_components=100, covariance_type='spherical', weight_concentration_prior=0.0--01/100, verbose=2).fit(X) # Instantiate and fit the model
     print('Converged:',GMM.converged_) # Check if the model has converged
     return GMM
 
@@ -40,21 +40,13 @@ def draw_ellipse(position, covariance, ax=None, **kwargs):
 
     ax.add_patch(circ)
 
-def draw_circle(position, covariance, ax=None, **kwargs):
-    """Draw an ellipse with a given position and covariance"""
+def draw_circle(position, radius, ax=None, **kwargs):
+    """Draw a circle with a given position and covariance"""
     ax = ax or plt.gca()
 
-    # Convert covariance to principal axes
-    if covariance.shape == (2, 2):
-        U, s, Vt = np.linalg.svd(covariance)
-        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
-        height = 2 * np.sqrt(s)
-    else:
-        angle = 0
-        height = 2 * np.sqrt(covariance)
+    height = 2 * np.sqrt(radius)
 
-
-    circ = Ellipse(position, 2*height, 2*height, angle, **kwargs)
+    circ = Ellipse(position, 2*height, 2*height, **kwargs)
     circ.set_facecolor('red')
     circ.set_edgecolor('red')
 
@@ -66,7 +58,9 @@ def make_plot(x, y, gmm, im):
     ax0.scatter(x,y)
     ax0.invert_yaxis()
     w_factor = 0.2 / gmm.weights_.max()
+    print(len(gmm.covariances_))
     for pos, covar, w in zip(gmm.means_, gmm.covariances_, gmm.weights_):
+        print(covar)
         draw_circle(pos, covar, alpha=w * w_factor)
 
     plt.show()
