d95d4d00556361bf658908655eea3ddcb13d002a
John Stratton <strattja@whitman.edu>
2020-09-25 12:15:35 -0700

    Orientation estimation used for initial alignment

--- src/image_alignment.py
+++ src/image_alignment.py
@@ -8,40 +8,64 @@ from skimage import io
 
 import image_utilities as img_util
 
+def shapeStatistics(shape_image):
+    moments = cv2.moments(shape_image)
+    center = moments["m10"]/moments["m00"], moments["m01"]/moments["m00"]
+    area = moments["m00"]
+    u20 = moments["mu20"]/moments["m00"]
+    u02 = moments["mu02"]/moments["m00"]
+    u11 = moments["mu11"]/moments["m00"]
+    angle = (180 / np.pi) * 0.5 * np.arctan(2*u11 / (u20 - u02))
+
+    #The angle given here will always be in the range [-45,45], whichever axis falls 
+    # within that range. To make sure we get the major axis, we need use use this method:
+    #  Citation: http://breckon.eu/toby/teaching/dip/opencv/SimpleImageAnalysisbyMoments.pdf
+    if (u20 - u02) < 0:
+        if u11 > 0:
+            angle += 90
+        else:
+            angle -=90
+
+    #Straighten the shape so that we can figure out which way is the "head" based on the aligned 
+    # third moment (e.g. skewness)
+    rotation = cv2.getRotationMatrix2D(center, -angle, 1).astype(np.float32)
+    aligned = cv2.warpAffine(shape_image, rotation, shape_image.shape, flags=cv2.INTER_LINEAR)
+    flat_moments = cv2.moments(aligned)
+    if flat_moments["mu30"] < 0:
+        angle += 180
+
+    length = np.sqrt(2*(u20 + u02 + np.sqrt(4*(u11**2) + (u20-u02)**2)))
+    width = np.sqrt(2*(u20 + u02 - np.sqrt(4*(u11**2) + (u20-u02)**2)))
+    return (center, area, angle, length, width)
+
+
 def match_images(petal_image, vein_image, s1, s2):
     sz = petal_image.shape
     #Consruct an initial guess of the transformation required to align the two images
-    PetalMoments = cv2.moments(s1)
-    PetalCenter = (PetalMoments["m10"]/PetalMoments["m00"], PetalMoments["m01"]/PetalMoments["m00"])
-    VeinMoments = cv2.moments(s2)
-    VeinCenter = (VeinMoments["m10"]/VeinMoments["m00"], VeinMoments["m01"]/VeinMoments["m00"])
-    PetalArea = PetalMoments["m00"]
-    VeinArea = VeinMoments["m00"]
+    (PetalCenter, PetalArea, PetalAngle, PetalLength, PetalWidth) = shapeStatistics(s1)
+    (VeinCenter, VeinArea, VeinAngle, VeinLength, VeinWidth) = shapeStatistics(s2)
     scale = VeinArea/PetalArea
     number_of_iterations = 100
     termination_eps = 1e-5
     criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
-    best_cc = 0
-    best_transform = None
-    for rotation in np.arange(0, 360, 90):
-        warp_matrix = cv2.getRotationMatrix2D(PetalCenter, rotation, scale).astype(np.float32)
-        warp_matrix[0][2] += VeinCenter[0]-PetalCenter[0]
-        warp_matrix[1][2] += VeinCenter[1]-PetalCenter[1]
-        try:
-            (cc, final_warp) = cv2.findTransformECC(s1,s2,warp_matrix, cv2.MOTION_AFFINE, criteria)
-        except (cv2.error):
-            cc = 0
-        if cc > best_cc:
-            best_cc = cc
-            best_transform = final_warp
-        if best_cc > 0.99:
-            break
-        
-    if best_cc < 0.9:
+    AngleDifference = VeinAngle - PetalAngle
+
+    warp_matrix = cv2.getRotationMatrix2D(PetalCenter, AngleDifference, scale).astype(np.float32)
+    warp_matrix[0][2] += VeinCenter[0]-PetalCenter[0]
+    warp_matrix[1][2] += VeinCenter[1]-PetalCenter[1]
+    test = cv2.warpAffine(cv2.bitwise_and(vein_image,s2), warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    #io.imshow_collection([s1, test])
+    #io.show()
+    try:
+        (cc, final_warp) = cv2.findTransformECC(s1,s2,warp_matrix, cv2.MOTION_AFFINE, criteria)
+    except (cv2.error):
+        cc = 0
+
+    if cc < 0.9:
         print("Error: can't find satisfactory alignment, displaying masks for debug")
         io.imshow_collection([s1, s2])
         io.show()
-    elif best_cc == 0:
+    elif cc == 0:
         raise ValueError("Cannot find any alignment for the images provided.")
     return cv2.warpAffine(cv2.bitwise_and(vein_image,s2), final_warp, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
 
