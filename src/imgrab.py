13738e75beee5a4c20f0453c092214378116d345
elsayeaa <64815927+elsayeaa@users.noreply.github.com>
2020-12-19 12:05:06 +0200

    Fixing FileNotFound error in imgrab.py

--- src/imgrab.py
+++ src/imgrab.py
@@ -1,10 +1,13 @@
 import principal_component as p
 import sys
+import os.path
 import cv2
 from skimage import io
 
 
 def main():
+    if not os.path.exists(sys.argv[1]): 
+        raise ValueError("The image files doesn't Exists. Please review your file path")
     image = cv2.imread(sys.argv[1])
     im = vein_image = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
     i = p.pca_to_grey(image)
