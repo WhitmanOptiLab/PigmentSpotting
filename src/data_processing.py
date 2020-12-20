0d0a9f07a8dc90f869a17f5876dc17146319f8f9
KauBoui <errolbucy@gmail.com>
2020-12-20 14:01:26 -0800

    Revert "Opencv Installation Guide"
    This reverts commit 81c366c92523dd0da71d29852f2d8ebe1cac04c0.

--- src/data_processing.py
+++ src/data_processing.py
@@ -7,7 +7,7 @@ On command line: python3 data_processing.py image_list filepath
 
 
 import sys
-import principal_component as pc
+import principlecomponent as pc
 import imghdr
 import cv2 as cv
 import os
