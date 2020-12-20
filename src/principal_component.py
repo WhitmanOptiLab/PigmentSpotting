81c366c92523dd0da71d29852f2d8ebe1cac04c0
KauBoui <errolbucy@gmail.com>
2020-12-20 13:59:54 -0800

    Opencv Installation Guide

--- src/principal_component.py
+++ src/principal_component.py
@@ -4,20 +4,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import image_shapes as shapes
 from skimage import io
+from sklearn.decomposition import PCA
 
 def pca_to_grey(image, mask, inverted=False):
-    x,y,z = image.shape
-    mat = image.reshape([x*y,z])
-    filter_array = mask.reshape([x*y])
-    pointlist = mat[filter_array > 0]
-    mean, eigenvectors = cv2.PCACompute(pointlist, mean=None)
-    axis = eigenvectors[0,:].reshape([3])
-
-    newmat = np.dot(mat.astype(np.float32) - mean, axis)
-    newpoints = newmat[filter_array > 0]
-    rescale = ((newmat - np.min(newpoints)) * 255.0 / (np.max(newpoints)-np.min(newpoints)))
-    print(np.min(rescale), np.max(rescale))
-    rescale = np.around(rescale).astype(np.uint8)
+    # x,y,z = image.shape
+    # mat = image.reshape([x*y,z])
+    # filter_array = mask.reshape([x*y])
+    # pointlist = mat[filter_array > 0]
+    # mean, eigenvectors = cv2.PCACompute(pointlist, mean=None)
+    # axis = eigenvectors[0,:].reshape([3])
+    # newmat = np.dot(mat.astype(np.float32) - mean, axis)
+    # print(np.min(newmat)), print(np.max(newmat))
+    # print(np.median(newmat)), print(np.mean(newmat))
+    # newpoints = newmat[filter_array > 0]
+    # print(np.min(newpoints)), print(np.max(newpoints))
+    # print(np.median(newpoints)), print(np.mean(newpoints))
+    # rescale = ((newmat - np.min(newpoints)) * 255.0 / (np.max(newpoints)-np.min(newpoints)))
+    # print(np.min(rescale), np.max(rescale))
+    # rescale = np.around(rescale).astype(np.uint8)
+    pca = PCA(n
 
     grey = rescale.reshape([x,y])
     if inverted:
@@ -25,8 +30,8 @@ def pca_to_grey(image, mask, inverted=False):
     pigment = cv2.bitwise_and(grey, mask)
     return pigment
 
-def create_point_cloud(image):
-    data = pca_to_grey(image)
+def create_point_cloud(image, mask):
+    data = pca_to_grey(image, mask)
     critical_points =[]
 
     for line in data:
@@ -50,20 +55,24 @@ def create_point_cloud(image):
 
     X = np.array(x)
     Y = np.array(y)
-    # plt.scatter(X,Y)
-    # plt.invert_yaxis()
-    # plt.show()
+    plt.scatter(X,Y)
+    plt.invert_yaxis()
+    plt.show()
     return X,Y, data
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Standalone usage: python principal_component.py input_filename.jpg [output_filename.jpg]")
-        sys.exit(1)
-    petal_image = cv2.imread(sys.argv[1])
+    # if len(sys.argv) < 2:
+    #     print("Standalone usage: python principal_component.py input_filename.jpg [output_filename.jpg]")
+    #     #sys.exit(1)
+    ## Just for now 
+    # petal_image = cv2.imread(sys.argv[1])
+    petal_image = cv2.imread("/Users/erb/Desktop/CS/PigmentSpotting/images/F1P102_Spot_Side2_200723_cropped.jpg")
     petal_shape = shapes.get_petal_shape(petal_image)
     result = pca_to_grey(petal_image, petal_shape, True)
-    if len(sys.argv) > 2:
-        cv2.imwrite(sys.argv[2], result)
-    else:
-        io.imshow(result)
-        io.show()
+    io.imshow(result)
+    io.show() 
+    # if len(sys.argv) > 2:
+    #     cv2.imwrite(sys.argv[2], result)
+    # else:
+    #     io.imshow(result)
+    #     io.show()
